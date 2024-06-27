30a9bc3dac9f91bb4009d1909204df57fe02fced
Max <max.heinzer397@gmx.ch>
2024-06-27 18:58:04 +0200

    feat: add module to get test data

---
+++ spec/data/entries.lua
@@ -0,0 +1,68 @@
+local entry_data = {}
+local source_names = { "lsp", "snippet", "path" }
+local kinds = {
+    "Text",
+    "Method",
+    "Function",
+    "Constructor",
+    "Field",
+    "Variable",
+    "Class",
+    "Interface",
+    "Module",
+    "Property",
+    "Unit",
+    "Value",
+    "Enum",
+    "Keyword",
+    "Snippet",
+    "Color",
+    "File",
+    "Reference",
+    "Folder",
+    "EnumMember",
+    "Constant",
+    "Struct",
+    "Event",
+    "Operator",
+    "TypeParameter",
+}
+
+---Creates an example source for testing
+---It won't have any entries set
+---@return neocomplete.internal_source
+local function example_source()
+    ---@type neocomplete.internal_source
+    local source = require("neocomplete.source").new({
+        complete = function()
+            return {}
+        end,
+    })
+    return source
+end
+
+---Returns `amount` completion items which have a `label` defined
+---@param amount integer
+---@return lsp.CompletionItem[]
+function entry_data.label_only(amount)
+    ---@type lsp.CompletionItem[]
+    local ret = {}
+    for i = 1, amount do
+        table.insert(ret, { label = "test" .. i })
+    end
+    return ret
+end
+
+---Returns `amount` completion items which have a `label`, `source`, and `kind`
+---@param amount integer
+---@return lsp.CompletionItem[]
+function entry_data.minimal(amount)
+    ---@type lsp.CompletionItem[]
+    local ret = {}
+    for i = 1, amount do
+        table.insert(ret, { label = "test" .. i, kind = (i % #kinds) + 1, source = example_source() })
+    end
+    return ret
+end
+
+return entry_data
