7e3d4df5d01aeaabbe2be9d589f53cf5c7afefee
Max <max.heinzer397@gmx.ch>
2024-07-14 16:21:55 +0200

    feat: add default highlights for kind

--- lua/neocomplete/highlights.lua
+++ lua/neocomplete/highlights.lua
@@ -11,6 +11,38 @@ local hl = function(...)
     vim.api.nvim_set_hl(0, ...)
 end
 
+local kinds = {
+    Text = "@string",
+    Method = "@function.method",
+    Function = "@function",
+    Constructor = "@constructor",
+    Field = "@property",
+    Variable = "@variable",
+    Class = "@type",
+    Interface = "@type",
+    Module = "@module",
+    Property = "@property",
+    Unit = "@constant",
+    Value = "@boolean",
+    Enum = "@constant",
+    Keyword = "@keyword",
+    Snippet = "@keyword",
+    Color = "@string.special",
+    File = "@string.special.url",
+    Reference = "@property",
+    Folder = "@string.special.url",
+    EnumMember = "@variable.member",
+    Constant = "@constant",
+    Struct = "@type",
+    Event = "@string.special",
+    Operator = "@operator",
+    TypeParameter = "@type",
+}
+
+for name, group in pairs(kinds) do
+    hl(string.format("@neocomplete.type.%s", name), { link = group, default = true })
+end
+
 hl("@neocomplete", { link = "Normal", default = true })
 hl("@neocomplete.type", { link = "Normal", default = true })
 hl("@neocomplete.selected", { link = "Visual", default = true })
