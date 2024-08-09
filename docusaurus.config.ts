c79950cbd9fd0a10cab1ffb38f087377652e67d8
Max <max.heinzer397@gmx.ch>
2024-08-09 22:01:28 +0200

    feat: use commonmark

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,6 +21,10 @@ const config: Config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
+  markdown: {
+    format: 'md'
+  },
+
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
