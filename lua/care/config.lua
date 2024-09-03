5663b3a472875e3b6b3a43a5121652f452e7bc2a
Max <max.heinzer397@gmx.ch>
2024-09-03 17:42:49 +0200

    feat: add type annotation to config setup

--- lua/care/config.lua
+++ lua/care/config.lua
@@ -89,6 +89,7 @@ config.defaults = {
     end,
 }
 
+---@param opts care.config
 function config.setup(opts)
     if vim.tbl_isempty(config.options) then
         config.options = vim.tbl_deep_extend("force", config.defaults, opts or {})
