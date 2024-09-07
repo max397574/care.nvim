57e32b416d0bf4a9d0033e415accff3965826aca
Max <max.heinzer397@gmx.ch>
2024-09-07 18:29:46 +0200

    feat: default kind to text

--- lua/care/utils/lsp.lua
+++ lua/care/utils/lsp.lua
@@ -3,6 +3,9 @@ local lsp_utils = {}
 --- Gets the name for an lsp kind
 ---@param kind_number lsp.CompletionItemKind
 function lsp_utils.get_kind_name(kind_number)
+    if kind_number == nil then
+        return "Text"
+    end
     local lsp_kinds = {
         "Text",
         "Method",
