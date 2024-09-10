651c187bc792151ba29d8a781f0336ffc4a28d60
Max <max.heinzer397@gmx.ch>
2024-09-10 13:21:10 +0200

    feat: update minimal_init.lua

--- minimal_init.lua
+++ minimal_init.lua
@@ -1,80 +1,48 @@
-local root = vim.fn.fnamemodify("./.repro", ":p")
+vim.env.LAZY_STDPATH = ".repro"
+load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
 
--- set stdpaths to use .repro
-for _, name in ipairs({ "config", "data", "state", "cache" }) do
-    vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
-end
+require("lazy.minit").repro({
+    spec = {
+        {
+            "max397574/care.nvim",
+            config = function()
+                vim.keymap.set("i", "<c-n>", function()
+                    vim.snippet.jump(1)
+                end)
+                vim.keymap.set("i", "<c-p>", function()
+                    vim.snippet.jump(-1)
+                end)
+                vim.keymap.set("i", "<c-space>", function()
+                    require("care").api.complete()
+                end)
 
--- bootstrap lazy
-local lazypath = root .. "/plugins/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--single-branch",
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath,
-    })
-end
-vim.opt.runtimepath:prepend(lazypath)
+                vim.keymap.set("i", "<cr>", "<Plug>(CareConfirm)")
+                vim.keymap.set("i", "<c-e>", "<Plug>(CareClose)")
+                vim.keymap.set("i", "<tab>", "<Plug>(CareSelectNext)")
+                vim.keymap.set("i", "<s-tab>", "<Plug>(CareSelectPrev)")
 
--- install plugins
-local plugins = {
-    -- do not remove the colorscheme!
-    { "folke/tokyonight.nvim" },
-    {
-        "max397574/care.nvim",
-        dependencies = { "max397574/care-lsp" },
-        config = function()
-            vim.keymap.set("i", "<c-n>", function()
-                vim.snippet.jump(1)
-            end)
-            vim.keymap.set("i", "<c-p>", function()
-                vim.snippet.jump(-1)
-            end)
-            vim.keymap.set("i", "<c-space>", function()
-                require("care").api.complete()
-            end)
+                vim.keymap.set("i", "<c-f>", function()
+                    if require("care").api.doc_is_open() then
+                        require("care").api.scroll_docs(4)
+                    else
+                        vim.api.nvim_feedkeys(vim.keycode("<c-f>"), "n", false)
+                    end
+                end)
 
-            vim.keymap.set("i", "<cr>", "<Plug>(CareConfirm)")
-            vim.keymap.set("i", "<c-e>", "<Plug>(CareClose)")
-            vim.keymap.set("i", "<tab>", "<Plug>(CareSelectNext)")
-            vim.keymap.set("i", "<s-tab>", "<Plug>(CareSelectPrev)")
-
-            vim.keymap.set("i", "<c-f>", function()
-                if require("care").api.doc_is_open() then
-                    require("care").api.scroll_docs(4)
-                else
-                    vim.api.nvim_feedkeys(vim.keycode("<c-f>"), "n", false)
-                end
-            end)
-
-            vim.keymap.set("i", "<c-d>", function()
-                if require("care").api.doc_is_open() then
-                    require("care").api.scroll_docs(-4)
-                else
-                    vim.api.nvim_feedkeys(vim.keycode("<c-f>"), "n", false)
-                end
-            end)
-        end,
+                vim.keymap.set("i", "<c-d>", function()
+                    if require("care").api.doc_is_open() then
+                        require("care").api.scroll_docs(-4)
+                    else
+                        vim.api.nvim_feedkeys(vim.keycode("<c-f>"), "n", false)
+                    end
+                end)
+            end,
+        },
+        {
+            "neovim/nvim-lspconfig",
+            config = function()
+                require("lspconfig")["lua_ls"].setup({})
+            end,
+        },
     },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require("lspconfig")["lua_ls"].setup({})
-        end,
-    },
-    -- =========TODO=============
-    -- your lspconfig stuff here
-    -- =========TODO=============
-    -- add any other pugins here
-}
-require("lazy").setup(plugins, {
-    root = root .. "/plugins",
 })
-
--- add anything else here
-vim.opt.termguicolors = true
--- do not remove the colorscheme!
-vim.cmd([[colorscheme tokyonight]])
